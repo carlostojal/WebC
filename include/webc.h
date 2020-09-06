a3be1835c77a6cd8d5931ba8b7a3aaa0e9b3e108
Carlos Tojal <carlos.tojal@hotmail.com>
2020-09-06 14:45:26 +0100

    comments

---
+++ include/webc.h
@@ -0,0 +1,15 @@
+
+/*
+ * WebC
+ *
+ * include/webc.h
+ *
+ * This is the library that must be
+ * included.
+ *
+ * 
+ * Carlos Tojal (carlostojal)
+ *
+ */
+
+#include "typedefs.h"
--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
+
+/*
+ * WebC
+ *
+ * src/main.c
+ *
+ * This is an example implementation
+ * of the library. 
+ *
+ * 
+ * Carlos Tojal (carlostojal)
+ *
+ */
+
 #include <stdio.h>
+
+#include "include/webc.h"
 
 int main() {
   printf("Hello World\n");
